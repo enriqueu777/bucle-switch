0878bce92e6fb19df66708522db617c1321ee450
Quique <eutreror@bcds.es>
2024-10-06 17:52:33 +0200

    inicio proyecto

---
+++ Switch/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Net.Mime;
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+        Console.WriteLine("Introduce el nuemero de mes para el calculo de la comision");
+
+
+        int mes = Int32.Parse(Console.ReadLine());
+
+        switch (mes) {
+
+
+            case 1:
+                Console.WriteLine("Mes escogido Enero");
+
+                break;
+
+
+            case 2:
+                Console.WriteLine("Mes escogido Febrero");
+
+                break;
+
+            case 3:
+                Console.WriteLine("Mes escogido Marzo");
+
+                break;
+
+
+
+            case 4:
+                Console.WriteLine("Mes escogido Abril");
+
+                break;
+
+            case 5:
+                Console.WriteLine("Mes escogido Mayo");
+
+                break;
+
+            case 6:
+                Console.WriteLine("Mes escogido Junio");
+
+                break;
+
+
+            case 7:
+                Console.WriteLine("Mes escogido Julio");
+
+                break;
+
+
+            case 8:
+                Console.WriteLine("Mes escogido Agosto");
+
+                break;
+
+
+            case 9:
+                Console.WriteLine("Mes escogido Septiembre");
+
+                break;
+
+
+            
+
+
+           
+                break;
+            default:
+
+                Console.WriteLine("La opcion elegida no es correcta");
+                break;
+
+
+
+
+
+
+        }
+    }
+
+}
